ef8c42a446d7aa497434e08ba047bd3122a9404f
Srishty Mangutte <srishtymangutte@gmail.com>
2024-02-26 02:50:10 +0000

    feat: wrote code for url shortner

---
+++ collection/urlDatabase.js
@@ -0,0 +1,6 @@
+var urlDatabase =[{
+    url:'https://github.com/Srish-ty/URL-shortner',
+    code:123
+}]
+
+module.exports = urlDatabase;
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const urlDatabase = require('./collection/urlDatabase');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded bodies
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +22,33 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+// solution for project
+function generateShortUrl() {
+  return Math.random().toString(36).substr(2, 7); // Example: Generate a random 7-character string
+}
+
+app.post('/api/shorturl',(req,res)=>{
+  var given_url= req.body.url;
+  var short_code= generateShortUrl();
+  const entry = {url: given_url, code:short_code}
+
+  urlDatabase.push(entry);
+  res.json({original_url:given_url, short_url:short_code})
+})
+
+// shortened url
+app.get('/api/shorturl/:shortUrl',(req,res)=>{
+  const shortUrl = req.params.shortUrl; 
+  const entry = urlDatabase.find(entry => entry.code === shortUrl);
+  if (entry && entry.url) {
+    res.redirect(entry.url); 
+  } else {
+    res.status(404).json({ error: 'Shortened URL not found' }); 
+  }
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
